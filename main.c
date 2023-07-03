143b1beb20c47c20ad9ca06939f3f1dc15404b52
Akozikhodzha <akozikhodzha@gmail.com>
2023-07-03 13:53:48 +0700

    2nd commit

--- main.c
+++ main.c
@@ -11,6 +11,7 @@ int main(void)
 
     char str[] = "Hello world!";
     int length = strlen(str);
+    int size = sizeof(str) / sizeof(str[0]); // размер входного массива
 
     char characters[length];
     
@@ -27,27 +28,46 @@ int main(void)
     }
     printf("\n");
 
-    char hexCode[length*2+1];
+    int counter = 0; // счетчик для записи преобразованных символов в массив
+    char hexCode[size*2];
 
-    asciiToHex(one, length, hexCode);
+    to_base_ascii(one, hexCode);
 
     printf("\n16-ричные коды символов:\n");
     for (int i = 0; i < length; i++) {
         printf("%02x ", characters[i]);
     }
     printf("\n");
+
+    printf("%s\n", hexCode); // выводим преобразованные символы
+
+    int array_length = length / 2; // длина результирующего массива
+    char result[array_length][3]; // создаем массив с элементами по 2 символа и дополнительным символом для нулевого завершения строки
+
+    // Разделяем строку на элементы массива
+    for (int i = 0; i < length; i += 2) {
+        strncpy(result[i / 2], hexCode + i, 2);
+        result[i / 2][2] = '\0'; // добавляем нулевой символ для завершения строки
+    }
+     // Выводим результат
+    for (int i = 0; i < length; i++) {
+        printf("%s ", result[i]);
+    }
+    printf("\n");
     
     struct Message message;
     message.AD = 21;
     message.ID = 2;
-    message.DATA=hexCode;
+    *message.DATA=result;
 
     printf("AD: %d\n", message.AD);
     printf("ID: %d\n", message.ID);
     printf("DATA:\n");
+    printf("%s\n", message.DATA);
     for (int i = 0; i < length; i++) {
         printf("%d ", message.DATA[i]);
     }
     printf("\n");
+
     return 0;
 }
--- main.h
+++ main.h
@@ -14,5 +14,33 @@ void asciiToHex(char* asciiCodes, int size, char* hexResult) {
     }
 }
 
+void to_base_ascii(const char *input, char *result) {
+    char base_range[] = "0123456789ABCDEF";
+    int size = strlen(input);
 
+    for (int i = 0; i < size; i++) {
+        int value = input[i];
+        int p = 0;
 
+        if (value < 0) {
+            value += 256; // добавляем 256 для поддержки отрицательных значений символов ASCII
+        }
+
+        do {
+            result[i * 2 + p++] = base_range[value % 16];
+            value /= 16;
+        } while (value > 0);
+
+        // Добавляем ведущий ноль, если необходимо
+        if (p < 2) {
+            result[i * 2 + p++] = '0';
+        }
+        
+        // Развернуть результат
+        for (int j = 0; j < p / 2; j++) {
+            char temp = result[i * 2 + j];
+            result[i * 2 + j] = result[i * 2 + p - j - 1];
+            result[i * 2 + p - j - 1] = temp;
+        }
+    }
+}
